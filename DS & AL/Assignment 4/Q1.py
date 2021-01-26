f39f1cd66a3cde23c8a380c4389109adccecf171
Shanmukeshwar <shanmukeshwar09@gmail.com>
2021-01-26 20:58:53 +0530

    Create Q1.py

---
+++ DS & AL/Assignment 4/Q1.py
@@ -0,0 +1,16 @@
+list1 = [ 1, 2, 3, 4, 5, 6 ] 
+
+# Pops and removes the last element from the list 
+print(list1.pop()) 
+
+print("New List after pop : ", list1, "\n") 
+
+list2 = [1, 2, 3, ('cat', 'bat'), 4]
+
+print("New List after pop : ", list2, "\n") 
+
+list2.push('letsUpgrade') 
+
+# Print list 
+
+print("New List after Push : ", list2, "\n") 
